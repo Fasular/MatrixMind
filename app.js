b26dd159403e74354c4855ec371fb257abf21222
Fasular <fabiansuquilanda97@gmail.com>
2026-09-05 12:49:44 -0500

    send first version

--- app.js
+++ app.js
@@ -1639,7 +1639,7 @@ class MatrixMindApp {
     }
 
     saveSettings() {
-        const ip = this.esp32IpInput.value.trim() || '192.168.1.100';
+        const ip = this.esp32IpInput.value.trim() || '192.168.137.15';
         this.esp32Ip = ip;
         this.autoSend = this.autoSendToggle.checked;
 
